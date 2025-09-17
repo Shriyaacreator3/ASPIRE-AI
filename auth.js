1bf338f1b6061bd80ec3f68cf65ac9064ccb0a1d
keer7hi <keerthiprabakar07@gmail.com>
2025-09-17 17:48:01 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 import { auth ,googleProvider} from "firebase";
-import { createUserwithEmailandPassword,signInWithPopup } from "firebase/auth";
+import { createUserwithEmailandPassword,signInWithPopup,signOut} from "firebase/auth";
 import { useState } from "react";
 
 export const auth = () => {
@@ -21,6 +21,14 @@ export const auth = () => {
             console.error(err);
         }
     };
+    const logout = async ()=> {
+        try{
+        await signOut(auth);
+        }catch(err){
+            console.error(err);
+        }
+    };
+    
 
     <div class="login-container">
     <h2>aspirAI 🚀</h2>
@@ -28,6 +36,9 @@ export const auth = () => {
       <input type="email" id="email" placeholder="✉ Email" required onChange={(e)=>setEmail(e.target.value)}/>
       <input type="password" id="password" placeholder="🔑 Password" required onChange={(e)=>setPassword(e.target.value)} />
       <button type="submit" onClick={signIn}>Enter</button>
+      <button type="submit" onClick={signInWithGoogle}>Sign In With Google</button>
+      <button onClick={logout}>Logout</button>
+    
     
     </div>
 
