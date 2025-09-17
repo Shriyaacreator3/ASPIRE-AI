62eb46530be5dc73f3f6614bd2d39d094e5ae7b9
Veeksha Aluvala <vikki30.aluvala@gmail.com>
2025-09-17 16:26:06 +0530

    auth

--- auth.js
+++ auth.js
@@ -1,2 +1,22 @@
 import { auth } from "firebase";
 import { createUserwithEmailandPassword } from "firebase/auth";
+import { useState } from "react";
+
+export const auth = () => {
+    const [email,setEmail]=useState("");
+    const [password, setPassword]=useState("");
+
+    const signIn = async ()=> {
+        await createUserwithEmailandPassword(auth, email, password);
+    };
+
+    <div class="login-container">
+    <h2>aspirAI 🚀</h2>
+    
+      <input type="email" id="email" placeholder="✉ Email" required onChange={(e)=>setEmail(e.target.value)}/>
+      <input type="password" id="password" placeholder="🔑 Password" required onChange={(e)=>setPassword(e.target.value)} />
+      <button type="submit" onClick={signIn}>Enter</button>
+    
+    </div>
+
+}
--- firebase.js
+++ firebase.js
@@ -22,5 +22,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const auth= getAuth(app);
-const database=getDatabase(app);
+export const auth= getAuth(app);
+export const database=getDatabase(app);
