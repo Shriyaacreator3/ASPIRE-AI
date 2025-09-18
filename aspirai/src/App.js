4eb9142f8d9aac3d51e9e51b746515034fad240c
keer7hi <keerthiprabakar07@gmail.com>
2025-09-18 23:39:13 +0530

    Update App.js

--- aspirai/src/App.js
+++ aspirai/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {Login} from "./auth";
+import { Signup} from "./Signup";
 import {Home} from "./home";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />  
+        <Route path="/signup" element={<Signup />} />  
         <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
