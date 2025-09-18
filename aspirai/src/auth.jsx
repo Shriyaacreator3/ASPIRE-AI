d0b51d30801aa015630bc8bd2f7ad64d071aac5e
keer7hi <keerthiprabakar07@gmail.com>
2025-09-18 22:20:20 +0530

    Update auth.jsx

--- aspirai/src/auth.jsx
+++ aspirai/src/auth.jsx
@@ -1,7 +1,7 @@
 
 import "./style.css";
 import { auth } from "./firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithPopup,signOut} from "firebase/auth";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -19,6 +19,22 @@ export const Login = () => {
       catch(err) {
         alert(err.message);
       }
+    const signInWithGoogle = async()=>
+       try{
+        await signInWithPopup(auth,googleProvider);
+        navigate("/home");
+      }
+      catch(err) {
+        alert(err.message);
+      }
+     const logout = async()=>
+       try{
+        await signOut(auth);
+        navigate("/home");
+      }
+      catch(err) {
+        alert(err.message);
+      }
 
     };
  
@@ -29,8 +45,8 @@ export const Login = () => {
       <input type="email" id="email" placeholder="✉ Email" required onChange={(e)=>setEmail(e.target.value)}/>
       <input type="password" id="password" placeholder="🔑 Password" required onChange={(e)=>setPassword(e.target.value)} />
       <button type="submit" onClick={signIn}>Enter</button>
-     
-  
+      <button type="submit" onClick={signInWithGoogle}>Sign In With Google</button>
+      <button onClick={logout}>Logout</button>
     
     
     </div>
